8d0867a43853e04c8daf9ab4d7bdd42a5f8b3c4e
Allan Lavell <alavell@gmail.com>
2014-03-28 13:17:46 -0400

    initial everything

---
+++ points/Assets/_Scripts/InputRelay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputRelay : MonoBehaviour {
+	public KeyCode[] keys;
+
+	void KeyPressed(int index) {
+
+	}
+
+	void Update() {
+		int i = 0;
+		foreach (var key in keys) {
+			if (Input.GetKeyDown(key)) {
+				KeyPressed(i);
+			}
+			i++;
+		}
+	}
+}
